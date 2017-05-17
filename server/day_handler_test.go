02f69ffeb79771794dcb40e07a68d4d609a8550a
Norbert Melzer <norbert.melzer@cloudseeds.de>
2017-05-17 11:10:58 +0200

    make it really the Options that get tested

--- server/day_handler_test.go
+++ server/day_handler_test.go
@@ -1,25 +1,68 @@
 package server
 
 import (
-	"io/ioutil"
-	"log"
 	"net/http"
+	"sort"
+	"strings"
 	"sync"
 	"testing"
 )
 
-func TestDayOptions(t *testing.T) {
+func init() {
 	wg := new(sync.WaitGroup)
 	wg.Add(1)
 	go Start("localhost:9191", wg)
 	wg.Wait()
+}
 
-	req, _ := http.NewRequest("GET", "http://localhost:9191/day", nil)
-	c := new(http.Client)
-	req.Header["Accept"] = []string{"application/xml"}
-	rep, _ := c.Do(req)
+func TestDayOptions(t *testing.T) {
+	testCases := []struct {
+		url     string
+		methods []string
+	}{
+		{"http://localhost:9191/day", []string{"GET", "OPTIONS"}},
+	}
 
-	body, err := ioutil.ReadAll(rep.Body)
+	for _, testCase := range testCases {
+		req, _ := http.NewRequest("OPTIONS", testCase.url, nil)
+		c := new(http.Client)
+		resp, err := c.Do(req)
 
-	log.Fatalln(string(body), err)
+		if err != nil {
+			t.Errorf("There was an error while doing OPTIONS for %s:\n%v", testCase.url, err)
+		}
+
+		parsedMethods := strings.Split(resp.Header["Allow"][0], ",")
+		for i := range parsedMethods {
+			parsedMethods[i] = strings.Fields(parsedMethods[i])[0]
+		}
+
+		if act, exp, eq := checkOptions(parsedMethods, testCase.methods); !eq {
+			t.Errorf("Expected %s to allow %v, but it allowed %v.", testCase.url, exp, act)
+		}
+	}
+}
+
+func checkOptions(got, want []string) ([]string, []string, bool) {
+	act := make([]string, len(got))
+	exp := make([]string, len(want))
+
+	copy(act, got)
+	copy(exp, want)
+
+	sort.Strings(act)
+	sort.Strings(exp)
+
+	eq := len(act) == len(exp)
+	if eq {
+	loop:
+		for i := range act {
+			if act[i] != exp[i] {
+				eq = false
+				break loop
+			}
+		}
+	}
+
+	return act, exp, eq
 }
