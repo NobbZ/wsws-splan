fb0e7c5a7b572790a64106e7dfae278dc356bd24
Norbert Melzer <timmelzer@gmail.com>
2017-05-17 23:43:45 +0200

    reduce logging noise

--- server/day_handler.go
+++ server/day_handler.go
@@ -3,8 +3,6 @@ package server
 import (
 	"net/http"
 
-	"log"
-
 	"github.com/julienschmidt/httprouter"
 
 	"github.com/NobbZ/wsws-splan/splan"
@@ -12,13 +10,11 @@ import (
 
 func daysGetHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	days := splan.GetAllDays(0)
-	log.Println("In daysGetHandler")
 	enc := GetEncoderFromRequest(r, w)
 
 	enc.Encode(days)
 }
 
 func dayGetHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	log.Println("In dayGetHandler")
 	w.WriteHeader(http.StatusOK)
 }
